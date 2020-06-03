e7a5e78ebb4bc951743e3b1f31486f7f5de909bc
@kisrakap <kisrakap@gmail.com>
2020-06-04 00:20:01 +0800

    replace all files

---
+++ WEEK 1/DAY 4/Grade Students.js
@@ -0,0 +1,24 @@
+var nama ="Kisra";
+var nilai = 80;
+var nilaia = 'Mendapat Nilai A'
+var nilaib = 'Mendapat Nilai B'
+var nilaic = 'Mendapat Nilai C'
+var nilaid = 'Mendapat Nilai D'
+var nilaie = 'Mendapat Nilai E'
+var nilai0 = 'Nilai Invalid'
+
+if( nilai <= 100 && nilai >= 80 ){
+console.log (nama +' '+ nilaia)
+}
+else if( nilai < 65 && nilai >= 50 ){
+console.log (nama +' '+ nilaib)
+}
+else if( nilai < 50 && nilai >= 35 ){
+console.log (nama +' '+ nilaic)
+}
+else if( nilai < 35 && nilai >= 0 ){
+console.log (nama +' '+ nilaid)
+}
+else {
+console.log (nama + ' '+ nilai0)
+}
---
+++ WEEK 1/DAY 4/Graduates.js
@@ -0,0 +1,14 @@
+var nama ="Kisra";
+var nilai = 80;
+var absen = 3;
+var lulus = 'LULUS';
+var tidaklulus = 'TIDAK LULUS';
+
+if( nilai >= 70 && absen < 5){
+console.log (nama +' '+ lulus)
+}
+else {
+console.log (nama +' '+ tidaklulus)
+}
+
+
